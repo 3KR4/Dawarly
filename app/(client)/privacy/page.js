41fb5174e74c142c199562345340892d753774a3
3KR4 <mouhamedmahmoud820@gmail.com>
2025-12-14 14:35:45 +0200

    ww

--- app/(client)/privacy/page.js
+++ app/(client)/privacy/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import "@/styles/pages/privacy.css";
+import "@/styles/client/pages/privacy.css";
 
 export default function PrivacyPolicy() {
   return (
