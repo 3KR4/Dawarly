fb09410a5bd36ebbfe6d7ef1c77d5d589eaa2f59
3KR4 <mouhamedmahmoud820@gmail.com>
2025-12-14 15:16:29 +0200

    aaaa

--- app/(dashboard)/dashboard/page.js
+++ app/(dashboard)/dashboard/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import "@/styles/client/pages/home.css";
-import Navigations from "@/components/tools/Navigations";
 
 export default function DashBoard() {
   return (
